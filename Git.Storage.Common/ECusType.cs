4d75a12e208a5f83c9cff8f67489308c078d49cd
yanghao <zjyh16@hotmail.com>
2017-12-29 16:45:11 +0800

    存储解决方案；

---
+++ Git.Storage.Common/ECusType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Git.Storage.Common
+{
+    public enum ECusType
+    {
+        /// <summary>
+        /// 合作领用
+        /// </summary>
+        [Description("合作领用")]
+        Cooperation = 1,
+
+        /// <summary>
+        /// 潜在领用
+        /// </summary>
+        [Description("潜在领用")]
+        Potential = 2,
+
+        /// <summary>
+        /// 丢失领用
+        /// </summary>
+        [Description("丢失领用")]
+        Lost = 3,
+
+        /// <summary>
+        /// 虚拟领用
+        /// </summary>
+        [Description("虚拟领用")]
+        Invented = 4
+    }
+}
---
+++ Git.Storage.Common/ESupType.cs
@@ -0,0 +1,45 @@
+﻿/*******************************************************************************
+ * Copyright (C) Git Corporation. All rights reserved.
+ *
+ * Author: 情缘
+ * Create Date: 2014-08-11 18:52:24
+ *
+ * Description: Git.Framework
+ * http://www.cnblogs.com/qingyuan/
+ * Revision History:
+ * Date         Author               Description
+ * 2014-08-11 18:52:24       情缘
+*********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Git.Storage.Common
+{
+    public enum ESupType
+    {
+        /// <summary>
+        /// 虚拟供应商
+        /// </summary>
+
+        [Description("雷达设备供应商")]
+        RadarSupplier = 1,
+
+        [Description("自动站设备供应商")]
+        AwsSupplier = 2,
+
+        [Description("计算机网络设备供应商")]
+        ComputerSupplier = 3,
+
+        [Description("虚拟供应商")]
+        Invented = 4,
+
+        [Description("合作供应商")]
+        Cooperation = 5
+
+
+    }
+}
